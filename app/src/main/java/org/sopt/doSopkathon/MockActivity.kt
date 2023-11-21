ba3d2f917576bc21662b765d17d5440f120f6974
Sangho Kim <marchbreeze03@gmail.com>
2023-11-22 04:21:43 +0900

    [ADD/#1] manifest & MyApp 설정

---
+++ app/src/main/java/org/sopt/doSopkathon/MockActivity.kt
@@ -0,0 +1,11 @@
+package org.sopt.doSopkathon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MockActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mock)
+    }
+}
