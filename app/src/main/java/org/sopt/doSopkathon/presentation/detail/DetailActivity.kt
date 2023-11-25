6a14d9478b0183d8c313507d37f7a8de99122edc
lsakee <93514333+lsakee@users.noreply.github.com>
2023-11-26 02:54:33 +0900

    [UI/#9] : UI 임시 완성

--- app/src/main/java/org/sopt/doSopkathon/presentation/detail/DetailActivity.kt
+++ app/src/main/java/org/sopt/doSopkathon/presentation/detail/DetailActivity.kt
@@ -3,15 +3,47 @@ package org.sopt.doSopkathon.presentation.detail
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import org.sopt.doSopkathon.R
 import org.sopt.doSopkathon.databinding.ActivityDetailBinding
-import org.sopt.doSopkathon.util.base.BindingActivity
+import org.sopt.doSopkathon.util.base.NoHideBindingActivity
 
-class DetailActivity : BindingActivity<ActivityDetailBinding>(R.layout.activity_detail) {
+class DetailActivity : NoHideBindingActivity<ActivityDetailBinding>(R.layout.activity_detail) {
 
+    private lateinit var detailAdapter: DetailAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val data = intent.getIntExtra("data", 1)
 
+        detailAdapter = DetailAdapter()
+        binding.rvComment.adapter = detailAdapter
+        detailAdapter.submitList(
+            listOf(
+                MockComment("1", "tadsasdasd", "fqweqwe"),
+                MockComment("1", "tadsawdasdasdasdasd", "fqweqwe"),
+                MockComment("1", "tadasdasdasdsasdasd", "fqweqwe"),
+                MockComment("1", "tadsasdasd", "fqweqwe"),
+            )
+        )
+        spandableTextLayout()
+    }
+
+    private fun spandableTextLayout() {
+        binding.tvTitle.setOnClickListener {
+            val isDetail01Visible = binding.layoutDetail01.visibility == View.VISIBLE
+
+            with(binding) {
+                layoutDetail01.visibility = if (isDetail01Visible) View.GONE else View.VISIBLE
+                layoutDetail02.visibility = if (isDetail01Visible) View.VISIBLE else View.GONE
+                tvWriteDate1.visibility = if (isDetail01Visible) View.GONE else View.VISIBLE
+                tvWriteDate2.visibility = if (isDetail01Visible) View.VISIBLE else View.GONE
+
+                tvTitle.animate().apply {
+                    duration = 300
+                    rotation(if (isDetail01Visible) 0f else 180f)
+                }
+            }
+        }
     }
 
     private inline fun <reified T : Activity> navigateTo() {
---
+++ app/src/main/java/org/sopt/doSopkathon/presentation/detail/MockComment.kt
@@ -0,0 +1,7 @@
+package org.sopt.doSopkathon.presentation.detail
+
+data class MockComment(
+    val img : String,
+    val text : String,
+    val date : String
+)
